190fed9fb48ae8cc26860dc116637950c77e7bf7
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-12-12 14:14:43 -0300

    sttausBar

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { ThemeProvider } from 'styled-components/native';
 import { Todolist } from '@screens/Todolist';
 import theme from './src/theme';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({Inter_400Regular, Inter_700Bold});
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
       {
         fontsLoaded ?  <Todolist /> : <ActivityIndicator/>
       }
